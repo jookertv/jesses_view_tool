8c0139aeeaf78feaa428f8dd24270c2dd475f96c
Jordan Hudgens <jordan@devcamp.com>
2017-06-27 16:52:39 +0000

    Jesses tool initial commit

---
+++ lib/jesses_view_tool.rb
@@ -0,0 +1,5 @@
+require "jesses_view_tool/version"
+require "jesses_view_tool/renderer"
+module JessesViewTool
+  # Your code goes here...
+end
---
+++ lib/jesses_view_tool/version.rb
@@ -0,0 +1,3 @@
+module JessesViewTool
+  VERSION = "0.1.0"
+end
